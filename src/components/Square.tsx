5409c88ec849a13938d68977e10dc4303930e2c7
Humakt83 <urlact@yahoo.com>
2021-03-24 10:49:06 +0200

    Make Square 'gradient'

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -12,15 +12,37 @@ const Square: React.FC<Props> = ({color = 0}) => {
   const opacity = color === -1 ? 0 : 1;
 
   const View = styled.View`
-    width: 35px;
-    height: 35px;
+    width: 30px;
+    height: 30px;
     background: ${COLOR_MAP[Math.max(0, color)]};
     border: 1px solid black;
     opacity: ${opacity}
   `;
 
+  const GradientA = styled.View`
+    z-index: 2;
+    width: 10px;
+    height: 30px;
+    background: white;
+    opacity: 0.6;
+    margin-left: 10px;
+  ` 
+
+  const GradientB = styled.View`
+    z-index: 1;
+    width: 20px;
+    height: 30px;
+    background: white;
+    opacity: 0.3;
+    margin-left: 10px;
+  ` 
+
    return (
-     <View />
+    <View>
+      <GradientB>
+        <GradientA/>
+      </GradientB>
+    </View>
    );
 };
 
