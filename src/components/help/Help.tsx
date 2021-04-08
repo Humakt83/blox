c309822b1973fa4e0a7680d089c1e15136b89e3e
Humakt83 <urlact@yahoo.com>
2021-04-08 13:26:52 +0300

    Update help style

--- src/components/help/Help.tsx
+++ src/components/help/Help.tsx
@@ -2,30 +2,18 @@ import React from 'react';
 
 import styled from 'styled-components/native';
 import MainContainer from '../common/MainContainer';
+import Paragraph from './Paragraph';
 
-const Start = () => {
+const Help = () => {
 
   return (
     <MainContainer>
       <View>
         <Header>Help</Header>
-        <Paragraph>
-          Goal of the game is to fill as many squares on the board with your pieces as possible.
-        </Paragraph>
-        <Paragraph>
-          Winner is the player who has with filled most squares on the board with his or her pieces.
-          Solid plan is to use the larger pieces first as the smaller ones are easier to fit later. 
-        </Paragraph>
-        <Paragraph>
-          You have one active piece shown that you can rotate to fit the board better. 
-          Drag the active piece and drop it on a highlighted area on the board.
-          You can change the active piece by touching other pieces of the same color underneath it.
-          Opponent pieces are also shown in a list so you can plan easier what pieces to place.
-          Press the skip button if you cannot place a piece on the board.
-        </Paragraph>
-        <Paragraph>
-          Game ends after you have placed your last piece or no pieces can be placed on the board anymore.
-        </Paragraph>
+        <Paragraph content="Goal of the game is to fill as many squares on the board with your pieces as possible." />
+        <Paragraph content="Winner is the player who has with filled most squares on the board with his or her pieces. Solid plan is to use the larger pieces first as the smaller ones are easier to fit later." />
+        <Paragraph content="You have one active piece shown that you can rotate to fit the board better. Drag the active piece and drop it on a highlighted area on the board. You can change the active piece by touching other pieces of the same color underneath it. Opponent pieces are also shown in a list so you can plan easier what pieces to place. Press the skip button if you cannot place a piece on the board."/>
+        <Paragraph content="Game ends after you have placed your last piece or no pieces can be placed on the board anymore."/>
       </View>
     </MainContainer>
   );
@@ -35,7 +23,6 @@ const View = styled.View`
   display: flex;
   margin-top: 15px;
   align-items: center;
-  width: 90%;
   text-align: center;
 `;
 
@@ -45,8 +32,4 @@ const Header = styled.Text`
   margin-bottom: 15px;
 `;
 
-const Paragraph = styled.Text`
-  margin-bottom: 10px;
-`
-
-export default Start;
+export default Help;
---
+++ src/components/help/Paragraph.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styled from 'styled-components/native';
+import {COLORS} from '../../Constants';
+
+type Props = {
+  content: string
+};
+
+const Paragraph : React.FC<Props> = ({content}) => {
+
+  return (
+    <Block>
+      <Text>{content}</Text>
+    </Block>
+  );
+};
+
+const Block = styled.View`
+  margin-bottom: 7px;
+  padding: 5px;
+  background-color: ${COLORS.white};
+  border-radius: 3px;
+  width: 340px;
+`;
+
+const Text = styled.Text`
+  margin-bottom: 10px;
+`
+
+export default Paragraph;
